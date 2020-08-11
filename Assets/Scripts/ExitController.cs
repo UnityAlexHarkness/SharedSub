4adb973228e3088d0b1df4b5939184522c5443b5
Alex Harkness <alex.harkness@unity3d.com>
2020-08-11 08:46:57 -0700

    Initial commit

---
+++ Assets/Scripts/ExitController.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitController : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        SceneManager.LoadSceneAsync("Scenes/Main");
+    }
+}
